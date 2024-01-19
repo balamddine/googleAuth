007e00ed133305308c2d0f0a2714db91b6899354
Bassem.A.Alameddine <balamddine@gmail.com>
2024-01-19 11:34:34 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(cors());
 
-
+app.get("/",(req,resp)=>{
+    resp.send("this is the google auth server")
+})
 app.use('/', authRoutes);
 
 // Start the server
